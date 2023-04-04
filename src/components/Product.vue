83e2e981520db38c8d0b48b3a3a1601d7a2f0ef4
Esochi <sochiera.emilia@gmail.com>
2023-04-04 20:09:53 +0200

    Example components, not everything is implemented correctly yet

---
+++ src/components/Product.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Product } from '@/interfaces/Product';
+
+interface Props {
+product : Product
+}
+
+
+const props = defineProps<Props>();
+
+
+</script>
+
+<template>
+  <div>{{ product.title}}</div>
+  <div>{{ product.brand }}</div>
+  <div>{{ product.category }}</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Product from '@/components/Product.vue';
+import axios from 'axios';
+import type { Product as ProductType  } from '@/interfaces/Product';
+import { ref } from 'vue';
+
+const myProduct = ref()
+
+axios.get<ProductType>('https://dummyjson.com/product/1').then((response) => {
+myProduct.value = response.data
+})
+
+//1. Stwórz komponent HomePage, który będzie zawierał logike odpowiedzialna za wyswietlanie produktow
+//2. Wyświetl listę wszystkich produktów
+//3* Utwórz żądanie post przez axios do którego dasz dane produktu, obojętnie jakie
+
+const myTable = ["a", "b", "c"];
+
+
+</script>
+
+<template>
+  <main>
+
+    <p :key="productIndex" v-for="(product, productIndex) in myTable">{{ product }}</p>
+    <Product :product="myProduct" />
+  </main>
+</template>
