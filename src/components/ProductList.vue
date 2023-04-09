8981c65a8322b455187c467760afd6c95f0eb7c5
Esochi <sochiera.emilia@gmail.com>
2023-04-09 23:39:34 +0200

    productList

---
+++ src/components/ProductList.vue
@@ -0,0 +1,18 @@
+<script setup lang=ts>
+import Product from './Product.vue';
+import type { ProductsList } from '@/interfaces/ProductsList';
+import axios from 'axios';
+import { ref } from 'vue';
+
+let response = await axios.get<ProductsList>("https://dummyjson.com/products")
+
+let products = ref(response.data)
+
+
+</script>
+
+<template>
+
+<Product :key="productIndex" :product="product" v-for="(product, productIndex) in products.products "></Product>
+
+</template>
