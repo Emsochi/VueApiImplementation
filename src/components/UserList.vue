3ff24dee490e3f9c188db2327cef886d45d4643d
Esochi <sochiera.emilia@gmail.com>
2023-04-10 22:21:30 +0200

    import interfaces, axios, import ref and axios

---
+++ src/components/UserList.vue
@@ -0,0 +1,10 @@
+<script setup lang=ts>
+import type * as UserList from '@/interfaces/UserList'
+import axios from 'axios';
+import { ref } from 'vue';
+
+
+let response = await axios.get<UserList>("https://dummyjson.com/users");
+
+
+</script>
