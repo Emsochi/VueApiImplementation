97912dadcb81fde68dd35ec95ec2e0830805d57f
Esochi <sochiera.emilia@gmail.com>
2023-04-11 20:07:45 +0200

    create components quotes.vue

---
+++ src/components/Quotes.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { Quote } from '../interfaces/Quote';
+
+interface Props {
+quotes : Quote
+}
+
+const props = defineProps<Props>();
+
+
+</script>
+
+<template>
+  <div>{{ quotes.id}}</div>
+  <div>{{ quotes.quote}}</div>
+  <div>{{ quotes.author}}</div>
+</template>
