559442d7cf720690a8b8c02064cedac117295711
Esochi <sochiera.emilia@gmail.com>
2023-04-11 20:07:05 +0200

    create post comment

---
+++ src/components/PostComment.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { CommentDTO } from '@/interfaces/CommentDTO';
+import { ref } from 'vue';
+import axios from 'axios';
+
+
+async function postComment() {
+
+const newComment : CommentDTO = {
+    body: body.value,
+    postId: postId.value,
+    userId: userId.value
+}
+
+const created = await axios.post<Comment>("https://dummyjson.com/comments/add", newComment)
+
+console.log(created.data)
+
+}
+
+const body = ref()
+const postId = ref()
+const userId = ref()
+
+</script>
+
+<template>
+
+
+Body:
+<input type="text" v-model="body" />
+
+<br>
+Post ID:
+<input type="text" v-model="postId">
+
+<br>
+
+User ID:
+<input type="text" v-model="userId">
+
+<button @click="postComment">Post Comment</button>
+
+
+
+</template>
