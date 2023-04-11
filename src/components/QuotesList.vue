d3d8510fb919493aaa4485d60fc984fffa6ab1e6
Esochi <sochiera.emilia@gmail.com>
2023-04-11 20:08:07 +0200

    create components quotesList.vue

---
+++ src/components/QuotesList.vue
@@ -0,0 +1,18 @@
+<script setup lang=ts>
+import type { QuotesList } from '@/interfaces/QuotesList'; 
+import axios from 'axios';
+import { ref } from 'vue';
+import Quotes from './Quotes.vue';
+
+
+let response = await axios.get<QuotesList>("https://dummyjson.com/quotes")
+
+let quoteList = ref(response.data)
+
+</script>
+
+<template>
+
+    <Quotes :quotes="quotes" :key="quotesIndex" v-for="(quotes, quotesIndex) in quoteList.quotes"/>
+    
+</template>
