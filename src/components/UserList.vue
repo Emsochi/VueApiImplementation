0658eae82acd142a5314dcdd1a0a46c863cbf986
Esochi <sochiera.emilia@gmail.com>
2023-04-12 22:17:44 +0200

    create components UserList

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,17 +1,18 @@
 <script setup lang=ts>
-import type * as UserList from '@/interfaces/UserList'
+import type {UserList} from '@/interfaces/UserList'
 import axios from 'axios';
 import { ref } from 'vue';
+import User from './User.vue';
 
 
 let response = await axios.get<UserList>("https://dummyjson.com/users")
 
-let type users = ref(response.data)
+let users = ref(response.data)
 
 </script>
 
 <template>
 
-    <Product :key="productIndex" :product="UserList" v-for="(UserList, productIndex) in products.products "></Product>
+    <User :user="user" :key="userIndex" v-for="(user, userIndex) in users.users"/>
     
-    </template>
+</template>
