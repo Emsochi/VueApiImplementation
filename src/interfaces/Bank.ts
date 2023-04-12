b0747028c0a46866c181e746988156fa71b296c7
Esochi <sochiera.emilia@gmail.com>
2023-04-12 22:18:10 +0200

    interface Bank

---
+++ src/interfaces/Bank.ts
@@ -0,0 +1,9 @@
+      
+      export interface Bank {
+        cardExpire: string
+        cardNumber: string
+        cardType: string
+        currency: string
+        iban: string
+      }
+      
