08ddd00b802147cb98803b3cb971b3d5ba3598d9
Esochi <sochiera.emilia@gmail.com>
2023-04-13 23:34:38 +0200

    components user

---
+++ src/components/User.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type {User} from "../interfaces/User"
+
+interface Props {
+user : User
+}
+
+const props = defineProps<Props>();
+
+
+</script>
+
+<template>
+  <div>{{ user.firstName}}</div>
+  <div>{{ user.lastName}}</div>
+  <div>{{ user.age}}</div>
+</template>
